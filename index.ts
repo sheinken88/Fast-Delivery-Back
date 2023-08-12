c9f1193c0b92a0a0456bc261f14882af4386b14d
Nahuel <nahuelvenerus@gmail.com>
2023-08-11 23:58:44 -0300

    fix: cookie correct console logs

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import router from "./routes"
 import cookieParser from "cookie-parser";
+import cors from "cors"
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use("/", router);
 
 app.listen(PORT, () => {
