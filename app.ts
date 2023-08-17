b3cb882d76e59be518ffb44d76491dc3bb3ee697
Sebastian Heinken <sheinken@Sebastians-MacBook-Pro.local>
2023-08-17 16:29:53 -0300

    fix: Modified server config

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import router from './routes'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import { connectDB } from './config/db'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
@@ -9,5 +12,17 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 app.use('/', router)
+
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT ?? 3000
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error)
+        process.exit(1)
+    })
 
 export { app }
