1224f69cee79ad048b8260ebbd1cf750c306a8ad
PC <heinken.sebastian@gmail.com>
2023-09-06 10:25:07 -0300

    fix: Send user at login

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ app.use('/', router)
 
 connectDB()
     .then(() => {
-        const PORT = process.env.PORT ?? 3001
+        const PORT = process.env.PORT ?? 3000
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
