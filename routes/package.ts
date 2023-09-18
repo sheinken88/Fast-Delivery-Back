25372dad4c02fe2d044e55b2614f5f8dc3a32714
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-09-18 12:07:01 -0300

    feat: modified packages model to fit the mock

--- routes/package.ts
+++ routes/package.ts
@@ -4,10 +4,14 @@ import {
     get_all_packages,
     edit_package,
     delete_package,
+    get_pending_packages,
+    get_delivered_packages,
 } from '../controllers/package.controller'
 const packageRouter = express.Router()
 
 packageRouter.get('/', get_all_packages)
+packageRouter.get('/pending', get_pending_packages)
+packageRouter.get('/delivered', get_delivered_packages)
 packageRouter.post('/', create_package)
 packageRouter.put('/edit', edit_package)
 packageRouter.delete('/:id', delete_package)
